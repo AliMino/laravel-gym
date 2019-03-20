01b07b978d9c198bf3692559ce4c49a25eebc028
Ali <mino.ict@gmail.com>
2019-03-20 05:45:39 +0200

    create 'training_sessions' table

---
+++ database/migrations/2019_03_20_034312_create_training_sessions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrainingSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('training_sessions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->date('start_at');
+            $table->date('end_at');
+            $table->unsignedBigInteger('gym_id');
+            $table->unsignedBigInteger('package_id')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('training_sessions');
+    }
+}
