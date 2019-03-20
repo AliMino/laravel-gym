71ef25d35fe50b0b4f602af50fb8ada05ea4797e
mohamed elmansy <eng.mohamedelmansy94@gmail.com>
2019-03-21 00:41:28 +0200

    add coach model and migration (coaches table)

--- database/migrations/2019_03_20_222549_create_coach_table.php
+++ database/migrations/2019_03_20_222549_create_coach_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCityTable extends Migration
+class CreateCoachTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCityTable extends Migration
      */
     public function up()
     {
-        Schema::create('cities', function (Blueprint $table) {
+        Schema::create('coaches', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->timestamps();
