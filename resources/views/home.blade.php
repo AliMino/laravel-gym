e88f90d25405cfb809873f7a58e428783bb50a0b
Ali <mino.ict@gmail.com>
2019-03-22 04:13:14 +0200

    Add users table seeder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,4 +21,56 @@
     </div>
 </div>
 
+<span>
+    <h1>
+        <mark>
+            Roles
+        </mark>
+    </h1>
+    <h1>
+        User has admin role : 
+        @if(auth()->user() && auth()->user()->hasRole('admin'))
+            true
+        @else
+            false
+        @endif
+    </h1>
+    
+    <h1>
+        User has city manager role : 
+        @if(auth()->user() && auth()->user()->hasRole('city manager'))
+            true
+        @else
+            false
+        @endif
+    </h1>
+</span>
+
+<span>
+    <h1>
+        <mark>
+            Permissions
+        </mark>
+    </h1>
+    
+    <h1>
+        User has permission to add city manager: 
+        @if(auth()->user() && auth()->user()->hasPermissionTo('add city manager'))
+            true
+        @else
+            false
+        @endif
+    </h1>
+    
+    <h1>
+        User can add gym manager: 
+        @if(auth()->user() && auth()->user()->can('add gym manager'))
+            true
+        @else
+            false
+        @endif
+    </h1>
+</span>
+
+
 @endsection
