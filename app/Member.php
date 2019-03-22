31fea5e0199ffaa029cec167fefff8b751741332
mohamed elmansy <eng.mohamedelmansy94@gmail.com>
2019-03-22 19:40:35 +0200

    adding member img profile

--- app/Member.php
+++ app/Member.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
-    //
+    protected $fillable=[
+        'name',
+        'email',
+        'password',
+        'gender',
+        'date of birth',
+        'profile img'
+    ];
 }
