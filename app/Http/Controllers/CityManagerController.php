70cda4d587a3f8962f93afcde171e38cbbd72c59
EsraaKedwany <esraa.mohamed1006@gmail.com>
2019-03-23 19:56:28 +0200

    add CityManager.create view

--- app/Http/Controllers/CityManagerController.php
+++ app/Http/Controllers/CityManagerController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use App\User;
+use App\City;
+
 
 class CityManagerController extends Controller
 {
