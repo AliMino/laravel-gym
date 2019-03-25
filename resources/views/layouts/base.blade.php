844f3e002365bda69f3add99da00b0b4dbcd1856
Ali <mino.ict@gmail.com>
2019-03-25 06:25:49 +0200

    Add cities Preview after adding a city to a country

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -38,10 +38,11 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-<!-- Google Font -->
+    <!-- Google Font -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
     <!-- DataTables -->
     <link rel="stylesheet" href="../../bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+    @yield('styles')
 
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
