2364b0db4e0689d71add8ccecce989d4780d2e5a
Ali <mino.ict@gmail.com>
2019-03-29 04:23:53 +0200

    Change city_id to area_id in users table

--- database/migrations/2019_03_23_161537_add_area_id_to_users.php
+++ database/migrations/2019_03_23_161537_add_area_id_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddCityIdToUsers extends Migration
+class AddAreaIdToUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddCityIdToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('city_id')->nullable();
+            $table->unsignedBigInteger('area_id')->nullable()->default(0);
         });
     }
 
