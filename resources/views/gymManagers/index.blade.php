755806b70981357065d4c29906dee9cb6293c5b0
mohamed elmansy <eng.mohamedelmansy94@gmail.com>
2019-03-29 20:25:24 +0200

    gym manager confilct fixed

--- resources/views/gymManagers/index.blade.php
+++ resources/views/gymManagers/index.blade.php
@@ -14,6 +14,7 @@
                         </div>
                         <div class="box-body">
                             <table id="gymmanagers" class="table table-bordered table-striped">
+                                <meta name="csrf-token" content="{{ csrf_token() }}">
                                 <thead>
                                     <tr>
                                         <th>ID</th>
@@ -58,21 +59,13 @@
                     }
                 }, {
                     mRender: function (data, type, row) {
-                        return '<center><a href=""  id="delete" row_id="'+row.id+'" class="btn btn-danger"><i class="glyphicon glyphicon-remove"></i></a></center>'
+                        return '<a  href="#" class="delete" id="'+row.id+'"><buttontype="button" class="btn btn-block btn-danger btn-flat"> Delete </button></a>'
                     }
                 }, {
                     mRender: function (data, type, row) {
                         if(!row.banned_at)
                             return '<center><a href="/ban/'+row.id+'" class="btn btn-success" title="unbanned user"><i class="glyphicon glyphicon-ok-circle"></i></a></center> '
                         else
-<<<<<<< HEAD
-                            return '<center><a href="/unban/'+row.id+'"  class="btn btn-success" title="unban"><i class="glyphicon glyphicon-ok-circle"></i></a></center>'
-                    }   
-                },
-                ],
-            });
-        });
-=======
                             return '<center><a href="/unban/'+row.id+'" class="btn btn-danger" title="banned user"><i class="glyphicon glyphicon-ban-circle"></i></a></center> '
                             
                     }   
@@ -80,30 +73,30 @@
             ],
         } );
 
-        $(document).on('click', '.delete', function(){
-        var id = $(this).attr('row_id');
-        if(confirm("Are you sure you want to Delete ?"))
-        {
-            $.ajax({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                url:"/gymmanagers/"+id,
-                type:'DELETE',
-                success:function(data)
+
+            $(document).on('click', '.delete', function(){
+                var id = $(this).attr('id');
+                if(confirm("Are you sure you want to Delete this manager?"))
                 {
-                    alert("Coach deleted successfully");
-                    $('#gymmanagers').DataTable().ajax.reload();
+                    $.ajax({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        url:"/gymmanagers/"+id,
+                        type:'DELETE',
+                        success:function(data)
+                        {
+                            alert("manager deleted successfully");
+                            $('#gymmanagers').DataTable().ajax.reload();
+                        }
+                    })
                 }
-            })
-        }
-        else
-        {
-            return false;
-        }
-    });
+                else
+                {
+                    return false;
+                }
+            });
 
     } );
->>>>>>> 07f6d6c2f0d6c953ff36a54737dd2d226fc29354
     </script>
 @endsection
