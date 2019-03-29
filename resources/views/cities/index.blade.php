74bd8e91d8decf6fe42104692a612c1d3bb3d3d1
mohamed elmansy <eng.mohamedelmansy94@gmail.com>
2019-03-29 17:20:45 +0200

    Cities delete added

--- resources/views/cities/index.blade.php
+++ resources/views/cities/index.blade.php
@@ -10,6 +10,7 @@
                 <button class="btn btn-primary">Add new city</button>
             </a>
             <table id="datatable">
+                <meta name="csrf-token" content="{{ csrf_token() }}">
                 <thead>
                     <th>ID</th>
                     <th>City Name</th>
@@ -45,13 +46,41 @@
             "serverSide": true,
             "ajax":"/cities/datatable",
             "type":"get",
-            "columns": [ { "data": "id" }, { "data": "name" }, { "data": "countryName" }, {
+            "columns": [ { "data": "id" }, { "data": "name" }, { "data": "country.name" }, {
                     mRender: function (data, type, row) {
                         return '<a href="/cities/'+row.id+'/edit" class="table-edit" data-id="' + row.id + '"><button type="button" class="btn btn-block btn-warning">Edit</button></a>'
                     }
-                } ]
+                },{
+                mRender: function (data, type, row) {
+                    return '<a  href="#" class="delete" id="'+row.id+'"><buttontype="button" class="btn btn-block btn-danger btn-flat"> Delete </button></a>'
+                    }
+                }
+            ]
         });
-    });
+            $(document).on('click', '.delete', function(){
+                var id = $(this).attr('id');
+                if(confirm("Are you sure you want to Delete this city?"))
+                {
+                    $.ajax({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        url:"/cities/"+id,
+                        type:'DELETE',
+                        success:function(data)
+                        {
+                            alert("city deleted successfully");
+                            $('#datatable').DataTable().ajax.reload();
+                        }
+                    })
+                }
+                else
+                {
+                    return false;
+                }
+            });
+
+        });
     </script>
     
 @endsection
