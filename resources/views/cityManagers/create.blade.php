cf51affc681655b667ae72efaec4820bc13995a8
nadafarouk <nada.h.farouk@gmail.com>
2019-03-29 22:58:56 +0200

    updating create form

--- resources/views/cityManagers/create.blade.php
+++ resources/views/cityManagers/create.blade.php
@@ -48,7 +48,7 @@
                 <label for="exampleInputPassword1">City</label>
                 <select class="form-control" name="city_id">
                     @foreach($cities as $city)
-                    <option value="{{$city->id}}">{{$city->name}} - {{$city->country->name}}</option>
+                    <option value="{{$city->id}}">{{$city->name}} </option>
                     @endforeach
                 </select>
                 {{-- {{$city->country->name}} --}}
